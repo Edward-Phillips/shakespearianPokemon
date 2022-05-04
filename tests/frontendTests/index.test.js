dd3b6fde1dd4183cb6123b03d8874268a63d17fa
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:08 +0100

    Failing: it renders a pokemon image, name caption and description based on the pokemon selected

--- tests/frontendTests/index.test.js
+++ tests/frontendTests/index.test.js
@@ -1,33 +1,66 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import Home from '../../pages/index'
-import '@testing-library/jest-dom'
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "../../pages/index";
+import { enableFetchMocks } from "jest-fetch-mock";
 
-describe('Home', () => {
-  it('renders a heading about shakespearian pokemon', () => {
-    render(<Home />)
+enableFetchMocks();
 
-    const heading = screen.getByRole('heading', {
+const mockPokemonAPIRequest = (
+  pokemonName,
+  pokemonDescription,
+  pokemonImage,
+) =>
+  fetch
+    .mockResponseOnce(
+      JSON.stringify({
+        name: pokemonName,
+        image: pokemonImage,
+        description: pokemonDescription,
+      })
+    )
+
+describe("Home", () => {
+  it("renders a heading about shakespearian pokemon", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
       name: /Search for Pokemon, shakespearian style!/i,
-    })
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-
-  it('renders a searchbar', () => {
-    render(<Home />)
-
-    const searchbar = screen.getByLabelText('search')
-
-    expect(searchbar).toBeInTheDocument()
+    expect(heading).toBeInTheDocument();
   });
 
-  it('has a list of pokemon to choose from in searchbar', async() => {
-    render(<Home />)
+  it("renders a searchbar", () => {
+    render(<Home />);
 
-    const pokemonList = screen.getByLabelText('search')
-    fireEvent(pokemonList, new MouseEvent('click', { bubbles: true }))
-    fireEvent.change(pokemonList, {target: {value: 'nidoking'}})
-    const bulbasaur = screen.getByText('nidoking')
-    expect(bulbasaur).toBeInTheDocument();
-  })
-})
+    const searchbar = screen.getByLabelText("search");
+
+    expect(searchbar).toBeInTheDocument();
+  });
+
+  it("has a list of pokemon to choose from in searchbar", async () => {
+    render(<Home />);
+
+    const pokemonList = screen.getByLabelText("search");
+    fireEvent(pokemonList, new MouseEvent("click", { bubbles: true }));
+    fireEvent.change(pokemonList, { target: { value: "nidoking" } });
+    const nidoking = screen.getByText("nidoking");
+    expect(nidoking).toBeInTheDocument();
+  });
+
+  it("renders a pokemon display with an image, caption and description", async () => {
+    render(<Home />);
+
+    mockPokemonAPIRequest('nidoking', 'A dangerous poison gas drifts from its mouth.', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/293.png');
+
+    const pokemonList = screen.getByLabelText("search");
+    fireEvent(pokemonList, new MouseEvent("click", { bubbles: true }));
+    fireEvent.change(pokemonList, { target: { value: "nidoking" } });
+    const nidokingImage = await screen.findByRole("img");
+    const nidokingcaption = screen.findByRole('caption', {name: 'nidoking'});
+    const nidokingdescription = screen.findByText("A dangerous poison gas drifts from its mouth.");
+    waitFor(() => expect(nidokingImage.src).toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/293.png'))
+    waitFor(() => expect(nidokingcaption.textContent).toBe('nidoking'));
+    waitFor(() => expect(nidokingdescription).toBeInTheDocument());
+  });
+});
