2f46b7db761ef4789312491f227b318e18305c51
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:08 +0100

    add error handling to model requests

--- models/pokemon.model.js
+++ models/pokemon.model.js
@@ -1,7 +1,7 @@
-import {cache} from '../caches/pokemon.cache';
+import { cache } from "../caches/pokemon.cache";
 
 export default class pokemonModel {
-  constructor (name) {
+  constructor(name) {
     this.name = name;
     if (cache[name]) {
       this.description = cache[name].description;
@@ -12,37 +12,55 @@ export default class pokemonModel {
     this.cache = cache[name];
   }
 
-  parsePokemonDescription (data) {
-    return data.json().then(data=> data.flavor_text_entries[0].flavor_text.replace(
-      /(\r\n|\n|\r|\f)/gm,
-      " "
-    ));
+  parsePokemonDescription(data) {
+    return data
+      .json()
+      .then((data) =>
+        data.flavor_text_entries[0].flavor_text.replace(
+          /(\r\n|\n|\r|\f)/gm,
+          " "
+        )
+      );
   }
-  parsePokemonImage (data) {
-    return data.json().then(data=> data.sprites.other["official-artwork"].front_default);
+  parsePokemonImage(data) {
+    return data
+      .json()
+      .then((data) => data.sprites.other["official-artwork"].front_default);
   }
-  async getPokemonDescription () {
+  async getPokemonDescription() {
     if (this.description) {
       return this.description;
     }
-    const pokemonSpeciesInfo = fetch(
-      `https://pokeapi.co/api/v2/pokemon-species/${this.name}`
-    );
+    try {
+      const pokemonSpeciesInfo = fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${this.name}`
+      );
 
-    this.cache.description =  await pokemonSpeciesInfo.then((data) =>
-      this.parsePokemonDescription(data)
-    );
-    return await this.cache.description;
+      this.cache.description = await pokemonSpeciesInfo.then((data) =>
+        this.parsePokemonDescription(data)
+      );
+      return await this.cache.description;
+    } catch (e) {
+      console.log(e);
+      return "we searched far and wide but could not find this pokemon";
+    }
   }
 
-  async getPokemonImage () {
+  async getPokemonImage() {
     if (this.sprite) {
       return this.sprite;
     }
-    const pokemonInfo = fetch(`https://pokeapi.co/api/v2/pokemon/${this.name}`);
-    this.cache.sprite = await pokemonInfo.then((data) =>
-      this.parsePokemonImage(data)
-    );
-    return await this.cache.sprite;
+    try {
+      const pokemonInfo = fetch(
+        `https://pokeapi.co/api/v2/pokemon/${this.name}`
+      );
+      this.cache.sprite = await pokemonInfo.then((data) =>
+        this.parsePokemonImage(data)
+      );
+      return await this.cache.sprite;
+    } catch (e) {
+      console.log(e);
+      return "urlNotFound";
+    }
   }
-};
+}
