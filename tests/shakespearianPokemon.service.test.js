7557e12e64100ee8d00ce48fdd3ca40e169a400e
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:07 +0100

    Failing: shakespearian pokemon service should return the name of the pokemon, a shakespearian description of the pokemon and an image of the pokemon

---
+++ tests/shakespearianPokemon.service.test.js
@@ -0,0 +1,65 @@
+import shakespearianPokemonService from "../services/shakespearianPokemon.service";
+import { enableFetchMocks } from "jest-fetch-mock";
+import dotenv from "dotenv";
+
+enableFetchMocks();
+
+const mockPokemonAndShakespearTranslationRequests = (
+  pokemonName,
+  pokemonDescription,
+  pokemonImage,
+  translatedText
+) => {
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      name: pokemonName,
+      flavor_text_entries: [
+        {
+          flavor_text: pokemonDescription,
+        },
+      ],
+    })
+  );
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      sprites: {
+        other: {
+          "official-artwork": {
+            front_default: pokemonImage,
+          },
+        },
+      },
+    })
+  );
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      success: {
+        total: 1,
+      },
+      contents: {
+        translated: translatedText,
+      },
+    })
+  );
+};
+
+describe("shakespearianPokemon", () => {
+  it("should return the name of the pokemon, a shakespearian description of the pokemon and an image of the pokemon", async () => {
+    mockPokemonAndShakespearTranslationRequests(
+      "charizard",
+      "Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally.",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+      "Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally."
+    );
+    const shakespearianPokemon = await shakespearianPokemonService("charizard");
+    expect(shakespearianPokemon).toHaveProperty("name", "charizard");
+    expect(shakespearianPokemon).toHaveProperty(
+      "description",
+      "Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally."
+    );
+    expect(shakespearianPokemon).toHaveProperty(
+      "image",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
+    );
+  });
+});
