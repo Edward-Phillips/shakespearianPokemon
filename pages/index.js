6d41ef5aebbef01a7eb5df1afbb00e28ef7d47a1
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:09 +0100

    add super basic links to both pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Pokedex from "../components/Pokedex/Pokedex";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <title>Ye Olde Pokemon</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <Link href="/whosthatpokemon">Try and guess the pokemon!</Link>
         <Pokedex />
     </div>
   );
