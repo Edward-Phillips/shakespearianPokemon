13e874260fdbc1dbbb9a9844ffac2294dab48f21
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:07 +0100

    Refactor: remove log statments

--- services/pokemon.service.js
+++ services/pokemon.service.js
@@ -4,8 +4,6 @@ export default async function pokemonService(pokemonName) {
   const pokemonModelInstance = new pokemonModel(pokemonName);
   const description = pokemonModelInstance.getPokemonDescription(pokemonName);
   const image = pokemonModelInstance.getPokemonImage(pokemonName);
-  console.log(await description);
-  console.log(await image);
   return {
     description: await description,
     image: await image,
