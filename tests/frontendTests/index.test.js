8180d22b6e9fe63047d993057fadf721b6c04bb4
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:08 +0100

    Failing: has a list of pokemon to choose from in searchbar

--- tests/frontendTests/index.test.js
+++ tests/frontendTests/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Home from '../../pages/index'
 import '@testing-library/jest-dom'
 
@@ -11,5 +11,23 @@ describe('Home', () => {
     })
 
     expect(heading).toBeInTheDocument()
+  })
+
+  it('renders a searchbar', () => {
+    render(<Home />)
+
+    const searchbar = screen.getByLabelText('search')
+
+    expect(searchbar).toBeInTheDocument()
+  });
+
+  it('has a list of pokemon to choose from in searchbar', async() => {
+    render(<Home />)
+
+    const pokemonList = screen.getByLabelText('search')
+    fireEvent(pokemonList, new MouseEvent('click', { bubbles: true }))
+    fireEvent.change(pokemonList, {target: {value: 'nidoking'}})
+    const bulbasaur = screen.getByText('nidoking')
+    expect(bulbasaur).toBeInTheDocument();
   })
 })
