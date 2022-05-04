0bd827df693c7051fcf0bf353c23509152371d3e
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:07 +0100

    Passing: handler should include description of the pokemon in the response

--- pages/api/pokemon/[id].js
+++ pages/api/pokemon/[id].js
@@ -2,5 +2,6 @@ export default function pokemonHandler(req, res) {
   const { id } = req.query;
   res.status(200).json({
     name: id,
+    description: `A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.`,
   });
 }
