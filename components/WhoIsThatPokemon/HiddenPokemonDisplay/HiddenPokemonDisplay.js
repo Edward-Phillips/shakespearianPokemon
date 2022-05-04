58c6eb985448cf3f849dc8d8faec4884d6fa511d
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:08 +0100

    set up component for obscured pokemon outline

---
+++ components/WhoIsThatPokemon/HiddenPokemonDisplay/HiddenPokemonDisplay.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './HiddenPokemonDisplay.module.css';
+export default function HiddenPokemonDisplay({pokemonInfo, reveal}) {
+  return (
+    <section className={styles.outerContainer}>
+    <article className={styles.pokemonDisplayContainer}>
+      <figure className={styles.pokemonDisplayFigure}>
+        <img className={`${styles.pokemonDisplayImage} ${!reveal && styles.obscured}`} src={pokemonInfo?.sprite} alt={reveal?pokemonInfo.name : 'a hidden pokemon. what could it be?'} />
+        {reveal && <figcaption className={styles.pokemonDisplayCaption}>{pokemonInfo?.name}</figcaption>}
+      </figure>
+    </article>
+      <p className={styles.pokemonDisplayDescription}>{pokemonInfo?.description?.replace(`/[${pokemonInfo?.name[0].toLowerCase()},${pokemonInfo?.name[0].toUpperCase()}]`,"diiimg")}</p>
+    </section>
+  );
+}
