4f302249d53d10b8b0f158328f8287f8972c2363
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:09 +0100

    update styling

--- components/WhoIsThatPokemon/Cell/Cell.js
+++ components/WhoIsThatPokemon/Cell/Cell.js
@@ -85,8 +85,8 @@ export default function Cell({
             ? isInPosition
               ? "green"
               : "yellow"
-            : "white",
-          color: isInPosition && isInWord ? "white" : "black",
+            : "transparent",
+          color: "white",
         }}
       />
     </>
