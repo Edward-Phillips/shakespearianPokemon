822eacf852da089955a3986702207c34a459e8cb
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:08 +0100

    basic error catching on frontend

--- components/Pokedex/Pokedex.js
+++ components/Pokedex/Pokedex.js
@@ -14,7 +14,9 @@ export default function Pokedex() {
       if (pokemonCache[pokemonName]) {
         setCurrentPokemonInformation(pokemonCache[pokemonName]);
       } else {
-        fetch(process.env.POKEAPI_ADDRESS + pokemonName)
+        try {
+
+          fetch(process.env.POKEAPI_ADDRESS + pokemonName)
           .then((response) => response.json())
           .then((data) => {
             setCurrentPokemonInformation({
@@ -31,6 +33,9 @@ export default function Pokedex() {
               },
             });
           });
+        } catch (error) {
+          console.log(error);
+        }
       }
     }
   };
