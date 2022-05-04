5337fa8141a58310f6eafd7abf59295deac88b89
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:09 +0100

    count submissions to display losing screen when out of guesses.

--- components/WhoIsThatPokemon/GridRow/GridRow.js
+++ components/WhoIsThatPokemon/GridRow/GridRow.js
@@ -2,13 +2,13 @@ import React from "react";
 import {Grid} from "../Grid/Grid";
 import styles from './gridRow.module.css';
 
-export function GridRow({guesses, word, setIsCorrect, isCorrect}) {
+export function GridRow({guesses, word, setIsCorrect, isCorrect, incrementSubmitCount, reset}) {
   return (
     <div className={styles.guessesGrid} styles={{gridTemplateRows:`repeat(${guesses}, 1fr)`}}>
       {Array(guesses)
         .fill(0)
         .map((_, index) => {
-          return <Grid word={word} row={index} setIsCorrect={setIsCorrect} isCorrect={isCorrect} />;
+          return <Grid word={word} row={index} setIsCorrect={setIsCorrect} isCorrect={isCorrect} incrementSubmitCount={incrementSubmitCount} reset={reset} />;
         })}
     </div>
   );
--- components/WhoIsThatPokemon/WhoIsThatPokemon.js
+++ components/WhoIsThatPokemon/WhoIsThatPokemon.js
@@ -2,33 +2,85 @@ import React, { useEffect, useState } from "react";
 import { GridRow } from "./GridRow/GridRow";
 import availablePokemon from "../PokeSearch/availablePokemon";
 import HiddenPokemonDisplay from "./HiddenPokemonDisplay/HiddenPokemonDisplay";
+import styles from './WhoIsThatPokemon.module.css';
+import ReactModal from 'react-modal';
+import ScoreModal from "./ScoreModal/ScoreModal";
 
 export default function WhoIsThatPokemon() {
   const [thatPokemon, setThatPokemon] = useState(
     availablePokemon[Math.floor(Math.random() * availablePokemon.length)]
   );
 
+  const [isCorrect, setIsCorrect] = useState(false)
+  const [winCount, setWinCount] = useState(0);
+  const [submitCount, setSubmitCount] = useState(0);
+  const [guesses, setGuesses] = useState(6);
+  const [reset, setReset] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
   useEffect(() => {
-    if(thatPokemon.sprite) {
+    ReactModal.setAppElement('body')
+  }, [])
+
+  const handlePlayAgain = () => {
+    setReset(true);
+    setThatPokemon(availablePokemon[Math.floor(Math.random() * availablePokemon.length)]);
+    setIsCorrect(false);
+    setModalOpen(false);
+    setSubmitCount(0);
+  }
+
+  useEffect(() => {
+    if (thatPokemon.sprite && !reset) {
       return;
     }
     try {
-
       fetch(process.env.POKEAPI_ADDRESS + thatPokemon.value)
-      .then((response) => response.json())
-      .then((data) => {
-        setThatPokemon({
-          name: thatPokemon.label,
-          sprite: data.sprite,
-          description: data.description,
+        .then((response) => response.json())
+        .then((data) => {
+          setThatPokemon({
+            name: thatPokemon.label,
+            sprite: data.sprite,
+            description: data.description,
+          });
         });
-      });
+      setReset(false);
     } catch (error) {
       console.log(error);
     }
-  }, [thatPokemon]);
-  return <div>
-  <HiddenPokemonDisplay pokemonInfo={thatPokemon} reveal={false} />
-  <GridRow guesses={6} word={thatPokemon.name ?? "loading"} />;
-  </div>
+  }, [thatPokemon, reset]);
+
+  useEffect(() => {
+    if (isCorrect) {
+     setWinCount(winCount + 1);
+     setModalOpen(true);
+     setIsCorrect(false)
+    }
+  }, [isCorrect]);
+
+  useEffect(() => {
+    if(submitCount === guesses) {
+      setModalOpen(true);
+      setWinCount(0);
+    }
+  }, [submitCount]);
+  return (
+    <div className={styles.container}>
+      <HiddenPokemonDisplay
+        pokemonInfo={thatPokemon}
+        loading={!(thatPokemon.name ?? false)}
+        reveal={isCorrect}
+        setReset={setReset}
+      />
+      <GridRow
+        guesses={guesses}
+        word={thatPokemon.name ?? "loading"}
+        setIsCorrect={setIsCorrect}
+        isCorrect={isCorrect}
+        incrementSubmitCount={() => setSubmitCount(submitCount + 1)}
+        reset={reset}
+      />
+      <ReactModal isOpen={modalOpen}><ScoreModal closeModal={() => setModalOpen(false)} wins={winCount} lost={submitCount === guesses} playAgain={handlePlayAgain} /></ReactModal>
+    </div>
+  );
 }
