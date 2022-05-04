9b216effad30302c1c816739febe9551e0d931f9
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:07 +0100

    Passing: it should handle request errors gracefully

--- pages/api/pokemon/[id].js
+++ pages/api/pokemon/[id].js
@@ -2,10 +2,18 @@ import pokemonService from "../../../services/pokemon.service";
 
 export default async function pokemonHandler(req, res) {
   const { id } = req.query;
-  const pokemonServiceInstance = pokemonService(id);
-  res.status(200).json({
-    name: id,
-    description: await(pokemonServiceInstance).then(data=> data.description),
-    image: await(pokemonServiceInstance).then(data=> data.image),
-  });
+  try {
+
+    const pokemonServiceInstance = pokemonService(id);
+    
+    res.status(200).json({
+      name: id,
+      description: await(pokemonServiceInstance).then(data=> data.description),
+      image: await(pokemonServiceInstance).then(data=> data.image),
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
 }
