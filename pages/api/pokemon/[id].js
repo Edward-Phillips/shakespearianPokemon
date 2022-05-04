b194be4e3197c64857f44ed8df692d6a725d0cce
Edward Phillips <edd0ddi0@gmail.com>
2022-05-04 19:12:07 +0100

    Passing: should return a URL to the pokemon image

--- pages/api/pokemon/[id].js
+++ pages/api/pokemon/[id].js
@@ -1,9 +1,12 @@
 export default async function pokemonHandler(req, res) {
   const { id } = req.query;
-  const PokemonInfo = fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+  const pokemonSpeciesInfo = fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}`
+  );
+  const pokemonInfo = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   res.status(200).json({
     name: id,
-    description: await PokemonInfo.then((data) =>
+    description: await pokemonSpeciesInfo.then((data) =>
       data
         .json()
         .then((data) =>
@@ -12,6 +15,11 @@ export default async function pokemonHandler(req, res) {
             " "
           )
         )
+    ),
+    image: await pokemonInfo.then((data) =>
+      data
+        .json()
+        .then((data) => data.sprites?.other["official-artwork"]?.front_default)
     ),
   });
 }
