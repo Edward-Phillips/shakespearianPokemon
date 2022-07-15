758b7863258b31addbde2f17025dc9f2cc35d44f
Edward Phillips <edd0ddi0@gmail.com>
2022-07-15 13:19:17 +0100

    rework page structure to prioritise who is that pokemon

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import PokedexSearch from "../components/PokedexSearch/PokedexSearch";
-import Link from "next/link";
+import styles from "../styles/whosThatPokemon.module.css";
+import WhoIsThatPokemon from "../components/WhoIsThatPokemon/WhoIsThatPokemon";
 
-export default function Home() {
+export default function whosThatPokemon() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ye Olde Pokemon</title>
+        <title>Who is that Pokemon?</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Link href="/whosthatpokemon">Try and guess the pokemon!</Link>
-        <PokedexSearch />
+        <WhoIsThatPokemon />
     </div>
   );
 }
---
+++ pages/pokedex.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import PokedexSearch from "../components/PokedexSearch/PokedexSearch";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ye Olde Pokemon</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+        <PokedexSearch />
+    </div>
+  );
+}
