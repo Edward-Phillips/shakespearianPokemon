7055b451b3f4fcf2432e9de89e213d4b80ca8bdc
Edward Phillips <edd0ddi0@gmail.com>
2022-07-15 13:18:35 +0100

    format navbar, make display conditional on props

--- components/Pokedex/Navbar/Navbar.js
+++ components/Pokedex/Navbar/Navbar.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-
-export default function Navbar ({navbar}) {
+export default function Navbar({ navbar }) {
   return (
-    <>
-      <p style={{gridArea: 'text', backgroundColor: "black", color: "#00e1ff", textAlign: 'center', border: '2px solid lightgrey'}}>{navbar.title}</p><Link href={navbar.href}><a style={{gridArea: 'button', placeSelf: 'center'}}>></a></Link>
-    </>
-  )
+    <div style={{ display: navbar.display ?? "block" }}>
+      <p
+        style={{
+          gridArea: "text",
+          backgroundColor: "black",
+          color: "#00e1ff",
+          textAlign: "center",
+          border: "2px solid lightgrey",
+        }}
+      >
+        {navbar.title}
+      </p>
+      <Link href={navbar.href}>
+        <a style={{ gridArea: "button", placeSelf: "center" }}>></a>
+      </Link>
+    </div>
+  );
 }
